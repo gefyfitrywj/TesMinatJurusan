7c6ef6992142cb5fb90226239eb9f691b92f8152
gefyfitrywj <gefyfitry01@gmail.com>
2025-06-02 13:53:04 +0700

    fix: update load_models to accept 2 arguments

--- backend/model_loader.py
+++ backend/model_loader.py
@@ -1,11 +1,8 @@
-import joblib
-import os
+import pickle
 
-BASE_DIR = os.path.dirname(__file__)
-MODEL_SMA_PATH = os.path.join(BASE_DIR, 'models', 'rf_sma_model.pkl')
-MODEL_S1_PATH = os.path.join(BASE_DIR, 'models', 'rf_s1_model.pkl')
-
-def load_models():
-    model_sma = joblib.load(MODEL_SMA_PATH)
-    model_s1 = joblib.load(MODEL_S1_PATH)
+def load_models(model_sma_path, model_s1_path):
+    with open(model_sma_path, "rb") as f:
+        model_sma = pickle.load(f)
+    with open(model_s1_path, "rb") as f:
+        model_s1 = pickle.load(f)
     return model_sma, model_s1
